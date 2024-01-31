1b76792204df2fcf4e73397eba6d01bbab9202db
aaditeynair <aaditeyn@gmail.com>
2024-01-31 16:00:27 +0530

    chore: setup vite and express server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Running app on port 3000");
+});
