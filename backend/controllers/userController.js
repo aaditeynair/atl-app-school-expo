c3caf9b5dbf9d92590c3fdec0d6ea97e6068b723
aaditeynair <aaditeyn@gmail.com>
2024-02-03 10:30:02 +0530

    refactor: save user passwords as hashed strings

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,32 @@
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
 const UserController = {
   createUser: async (req, res) => {
     const { username, email, password } = req.query;
 
-    try {
-      const newUser = await User.create({ username, email, password });
-      res.status(201).json(newUser);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
+    bcrypt
+      .hash(password, 10)
+      .then((hashedPassword) => {
+        try {
+          User.create({
+            username,
+            email,
+            password: hashedPassword,
+          }).then((user) => {
+            res.status(201).json(user);
+          });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ error: "Internal Server Error" });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res
+          .status(500)
+          .json({ error: "Internal Sever Error during Password Hashing" });
+      });
   },
 };
 
