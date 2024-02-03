9015adf937d0a419ea281edb90351692a23f999f
aaditeynair <aaditeyn@gmail.com>
2024-02-03 10:07:37 +0530

    chore: create basic frontend view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,28 @@ function App() {
   return (
     <>
       <CssBaseline />
+      <div className="p-12">
+        <div className="flex justify-between">
+          <div>
+            <h1 className="text-5xl font-bold">Welcome Aadi</h1>
+            <p className="mt-1">What are we learning today?</p>
+          </div>
+          <div className="">
+            <a href="/signup">Sign Up</a>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 h-52 gap-12 mt-6">
+          <div className="">
+            <h1 className="text-2xl font-bold">Chapters</h1>
+          </div>
+          <div className="text-2xl font-bold">
+            <h1>Assignments</h1>
+          </div>
+        </div>
+
+        <div></div>
+      </div>
     </>
   );
 }
